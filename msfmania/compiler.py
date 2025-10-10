ded0ef2fc5654efb4d078359621bbd65cf72ae2f
lepotekil <52042546+lepotekil@users.noreply.github.com>
2025-10-10 22:15:25 +0200

    Optimized compilation

--- msfmania/compiler.py
+++ msfmania/compiler.py
@@ -2,6 +2,13 @@ from os import system, makedirs
 import shutil
 
 def compile(filename):
-    compiler = "x86_64-w64-mingw32-gcc"
-    system(f"{compiler} /tmp/main.c -o {filename}.exe")
-           
+    system(
+        f"x86_64-w64-mingw32-gcc "
+        f"-Wall -Wextra "                
+        f"-O2 -g "                       
+        f"-fno-exceptions -fno-asynchronous-unwind-tables "  
+        f"-static -static-libgcc -static-libstdc++ "
+        f"-mwindows "
+        f"-Wl,--nxcompat -Wl,--dynamicbase "
+        f"/tmp/main.c -o {filename}.exe"
+    )
